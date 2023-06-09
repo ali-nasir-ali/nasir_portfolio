e0d8cada8496bea588d21ff6dcec7fa88129e219
nasir ali <a.nasirali@outlook.com>
2023-06-09 15:19:35 +0100

    added the rest of the features

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import Header from "./Header";
-// import Footer from './Footer';
-// import MainSection from './MainSection';
+import Footer from "./Footer";
+import MainSection from "./MainSection";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
       <Header />
-      {/* 
       <MainSection />
-      <Footer />  */}
+      <Footer />
     </div>
   );
 }
